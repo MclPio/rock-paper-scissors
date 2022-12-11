44f89bdf3259c153a891869dba0304dbc496ac3b
MclPio <113801014+MclPio@users.noreply.github.com>
2022-12-11 11:09:10 -0500

    improve getPlayerChoice function. complete playRound function.
    getPlayerChoice will now account for upper case or lower case entries.
    The only issue is mistyping which will cause error and page refresh will
    be needed.

--- game.js
+++ game.js
@@ -1,55 +1,63 @@
 function getComputerChoice() {
-    //return random: rock,paper, scissors.
-    //1. randomly choose 1,2,3
-    //2. return chosen value
     let computerChoice;
-    const options = ['Rock', 'Paper', 'Scissors']
-    computerChoice = Math.floor(Math.random()*3);
-    return options[computerChoice];
+    const options = ['Rock', 'Paper', 'Scissors'];
+    computerChoice = Math.floor(Math.random()*3); //random number 0,1,2
+    return options[computerChoice]; //random choice rock paper scissors
 }
 
 function getPlayerChoice() {
-    choice = prompt("Choose Rock, Paper, Scissors:")
-    return choice;
+    var choice = prompt("Choose Rock, Paper, Scissors:"); //prompt player to type
+    firstLetter = (choice.slice(0,1)).toUpperCase(); //always uppercase first letter
+    otherLetters = (choice.slice(1)).toLowerCase(); //always lowercase rest of letters
+    cleanChoice = firstLetter + otherLetters; //return clean choice to pass on to game
+    return cleanChoice;
     }
     
 
 
-function rockPaperScissors(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let result; //define result
     
     if (playerSelection == "Rock" ) {
         if (computerSelection == "Rock") {
-            result = "Tie"
+            result = "Tie";
+            textResult = `You ${result}! ${playerSelection} = ${computerSelection}`;
             //if player rock, computer rock. tie
         } else if (computerSelection == "Paper") {
-            result = "Lose"
+            result = "Lose";
+            textResult = `You ${result}! ${computerSelection} beats ${playerSelection}`;
             //if player rock computer paper. lose
         } else if (computerSelection == "Scissors") {
-            result = "Win"
+            result = "Win";
+            textResult = `You ${result}! ${playerSelection} beats ${computerSelection}`;
             //if player rock computer scissor. win
         }
     } else if (playerSelection == "Paper") {
         if (computerSelection == "Paper") {
-            result = "Tie"
+            result = "Tie";
+            textResult = `You ${result}! ${playerSelection} = ${computerSelection}`;
             //if player paper computer paper tie.
         } else if (computerSelection == "Scissors") {
-            result = "Lose"
+            result = "Lose";
+            textResult = `You ${result}! ${computerSelection} beats ${playerSelection}`;
             //if player paper computer scissors lose.
         } else if (computerSelection == "Rock") {
-            result = "Win"
+            result = "Win";
+            textResult = `You ${result}! ${playerSelection} beats ${computerSelection}`;
             //if player paper computer rock win.
         }
     } else if (playerSelection == "Scissors") {
         if (computerSelection == "Scissors") {
-            result = "Tie"
-            textResult = `You ${result} ${playerSelection} ${computerSelection}`;
+            result = "Tie";
+            textResult = `You ${result}! ${playerSelection} = ${computerSelection}`;
             //if player scissors computer scissors tie.
         } else if (computerSelection == "Rock") {
-            result = "Lose"
+            result = "Lose";
+            textResult = `You ${result}! ${computerSelection} beats ${playerSelection}`;
             //if player scissors computer rock lose.
         } else if (computerSelection == "Paper") {
-            result = "Win"
+            result = "Win";
+            textResult = `You ${result}! ${playerSelection} beats ${computerSelection}`;
             //if player scissors computer paper win.
         }
     }
@@ -60,4 +68,14 @@ function rockPaperScissors(playerSelection, computerSelection) {
     //get player variable get computer variable print result from if statement.
 }
 
-console.log(rockPaperScissors(getPlayerChoice(), getComputerChoice()));
+function game() {
+    //call playRound
+    //for 5 rounds
+    //if player wins add 1 point to their score
+    //if computer wins add 1 point to their score
+}
+
+
+
+
+//console.log(playRound(getPlayerChoice(), getComputerChoice()));
